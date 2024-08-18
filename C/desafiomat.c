d5269bc5b1cedffc60669d31fb138c63bd111ec4
Caio <caiomand04@gmail.com>
2024-08-17 21:49:21 -0300

    vamarelo
    coloquei a cor amarelo antes do verde

--- C/desafiomat.c
+++ C/desafiomat.c
@@ -6,6 +6,7 @@
 
 void cabeca(char *x1, char *x2, char *x3, char *x4); // Declaração de função, para poder ficar depois da função int main
 void troca(char *t); // Declaração de função
+void amarelo(char *v); // Declaração de função
 
 int main(void) {
     char s1[10] = "Vermelho", s2[10] = "Verde", s3[10] = "Vermelho", s4[10] = "Vermelho"; // Declaração das variáveis que armazenam as cores de cada semáforo
@@ -13,22 +14,35 @@ int main(void) {
 
     for (;;) { // Início de um loop infinito para o controle dos semáforos
         cabeca(s1, s2, s3, s4); // Função para exibir as cores dos semáforos na tela
-        sleep(20); // Temporizador de 20s para simular a passagem do tempo
+        sleep(18); // Temporizador de 18s para simular a passagem do tempo
+        amarelo(s2); // Chama a função para trocar a cor do segundo semáforo para Amarelo
+        system("cls"); // Limpar tela 
+        cabeca(s1, s2, s3, s4); // Chama a função de novo para atualizar com as cores trocadas
+        sleep(2); // Temporizador de 2s para simular a passagem do tempo
         troca(s2); // Chama a função para trocar a cor do segundo semáforo 
         troca(s3); // Chama a função para trocar a cor do terceiro semáforo
         system("cls"); // Limpar tela 
         cabeca(s1, s2, s3, s4); // Chama a função de novo para atualizar com as cores trocadas
-        sleep(20); // Temporizador de 20s para simular a passagem do tempo
+        sleep(18); // Temporizador de 18s para simular a passagem do tempo
+        amarelo(s3); // Chama a função para trocar a cor do terceiro semáforo para Amarelo
+        system("cls"); // Limpar tela 
+        cabeca(s1, s2, s3, s4); // Chama a função de novo para atualizar com as cores trocadas
+        sleep(2); // Temporizador de 2s para simular a passagem do tempo
         troca(s3); // Chama a função para trocar a cor do terceiro semáforo
         troca(s1); // Chama a função para trocar a cor do primeiro semáforo
         troca(s4); // Chama a função para trocar a cor do quarto semáforo
-        system("cls"); // Limpar tela
+        system("cls"); // Limpar tela 
         cabeca(s1, s2, s3, s4); // Chama a função de novo para atualizar com as cores trocadas
-        sleep(40); // Temporizador de 40s para simular a passagem do tempo
+        sleep(38); // Temporizador de 38s para simular a passagem do tempo
+        amarelo(s1); // Chama a função para trocar a cor do primeiro semáforo para Amarelo
+        amarelo(s4); // Chama a função para trocar a cor do quarto semáforo para Amarelo
+        system("cls"); // Limpar tela 
+        cabeca(s1, s2, s3, s4); // Chama a função de novo para atualizar com as cores trocadas
+        sleep(2); // Temporizador de 2s para simular a passagem do tempo
         troca(s1); // Chama a função para trocar a cor do primeiro semáforo 
-        troca(s4); // Chama a função para trocar a cor do quarto semáforo
-        troca(s2); // Chama a função para trocar a cor do segundo semáforo
-        system("cls"); // Limpar tela
+        troca(s4); // Chama a função para trocar a cor do quarto semáforo 
+        troca(s2); // Chama a função para trocar a cor do segundo semáforo 
+        system("cls"); // Limpar tela 
     }
 }
 
@@ -45,4 +59,8 @@ void troca(char *t) { // Função para trocar a cor do semáforo: se a cor atual
     } else { // Senão
         strcpy(t, "Vermelho"); // Troca a cor para vermelho
     }
+}
+
+void amarelo(char *v) { // Função para trocar a cor do semáforo para Amarelo
+    strcpy(v, "Amarelo"); // Troca a cor para amarelo
 }
